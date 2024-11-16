9fb3a69fd8022c640e6d6b4acb6d556682f5a064
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2024-11-16 19:30:41 +0600

    GetProductById added

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -20,5 +20,11 @@ namespace HPlusSport.API.Controllers
         {
             return Ok(_context.Products.ToArray());
         }
+        [HttpGet("{id}")]
+        public ActionResult GetProductById(int id)
+        {
+            var product = _context.Products.Find(id);
+            return Ok(product);
+        }
     }
 }
