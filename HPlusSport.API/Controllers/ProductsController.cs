70af0ef52e38671e6a6011c6620dcef1e41e1cf0
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2024-11-16 19:22:42 +0600

    Action result is used

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -16,9 +16,9 @@ namespace HPlusSport.API.Controllers
             _context.Database.EnsureCreated();
         }
         [HttpGet]
-        public IEnumerable<Product> GetAllProducts()
+        public ActionResult GetAllProducts()
         {
-            return _context.Products.ToArray();
+            return Ok(_context.Products.ToArray());
         }
     }
 }
